c99549e6889f7664f152a3bef32f453f4083a26d
anupjungkarki <anupkarki2012@gmail.com>
2024-11-27 12:49:52 +0545

    Final Push

--- js/script.js
+++ js/script.js
@@ -1,25 +1,53 @@
 const navLinks = document.querySelectorAll('header nav a');
 const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+menuIcon.addEventListener('click',()=>{
+	menuIcon.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+})
 
 const activePage = () =>{
-	const barsBox = document.querySelector('.bars-box');
+	const header = document.querySelector('header');
+    const barsBox = document.querySelector('.bars-box');
 
-	navLinks.forEach(link =>{
-		link.classList.remove('active');
-	});
+   header.classList.remove('active');
+    setTimeout(() => {
+        header.classList.add('active');
+    }, 1100);
 
-	barsBox.classList.remove('active');
-	setTimeout(()=>{
+ 
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+
+
+    barsBox.classList.remove('active');
+    setTimeout(() => {
         barsBox.classList.add('active');
-	}, 1100);
-}
+    }, 1100);
+
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    menuIcon.classList.remove('bx-x');
+	navbar.classList.remove('active');
+
+};
 
 navLinks.forEach((link , idx) =>{
 	link.addEventListener('click', () =>{
 		if(!link.classList.contains('active')){
 			activePage();
 
-			link.classList.add('active')
+			link.classList.add('active');
+
+			setTimeout(() => {
+                 sections[idx].classList.add('active');
+            }, 1100);
 		}
 	});
 });
@@ -29,6 +57,10 @@ logoLink.addEventListener('click', () =>{
     if(!navLinks[0].classList.contains('active')){
     	activePage();
     	navLinks[0].classList.add('active');
+
+    	setTimeout(() => {
+            sections[0].classList.add('active');
+           }, 1100);
     }
 });
 
